be68a43a69b362e5a5da32fe9044964c11c52a49
Hritul <hritul.srivastava@gmail.com>
2023-01-07 11:46:01 +0530

    Events Over

--- src/comp/events/Events.jsx
+++ src/comp/events/Events.jsx
@@ -1,57 +1,116 @@
-import React from 'react'
-import './EventsStyle.css'
+import React from "react";
+import "./EventsStyle.css";
 
 const Events = () => {
-    return (
-      <div>
-            <section id="about">
-      <div className="wrapper">
-        <article>
-          <div className="title">
-            <h3>WHY JOIN E-CELL ?</h3>
-            <p className="separator" />
-          </div> 
-          <div className="desc full">
-            <h4 className="subtitle">We teach you the Art to Sell</h4>
-            <p>
-            I suppose the answer is very obvious. You might be very good at developing or competitive coding but, you are not good enough if you don’t know how to sell it. To sell is one of the most important skills one should have.
-            </p>
-            <p>
-            We, here at Entrepreneurship Cell, LNMIIT try to nurture the entrepreneurial spirit amongst the students in college. 
-            </p>
-          </div>
-          <div className="title">
-            <h3>Events</h3>
-            <p className="separator" />
-          </div>
-          <div className="desc">
-            <h4 className="subtitle">STARTUP WEEKEND JAIPUR</h4>
-            <p>
-            Startup Weekend aims to promote the Startup culture by providing an ideal environment for startup magic to happen. Surrounded by smart, passionate people and the right tools and approaches at your disposal, you’ll take giant leaps towards creating a business, becoming a founder, and connect with the right people and resources.
-            </p>
-            <p>
-              If you have some skills and think that they might be handy for somebody, then Techstars Startup Weekend is the right place for you.
-            </p>
-          </div>
-          <div className="desc">
-            <h4 className="subtitle">Also a designer.</h4>
-            <p>
-              I feel comfortable working with many Adobe products. Photoshop, Illustrator, InDesign,
-              Lightroom or Xd are some kind of industry standards and I love working with them. I'm
-              not limited to them, though: Gimp, Inkscape or Figma are also very valid applications
-              that I've been working with.
-            </p>
-            <p>
-              User interfaces, brochures, books, branding... You name it! As I mentioned, creating
-              pretty things is particularly important for me.
-            </p>
-          </div>
-          
-        </article>
-      </div>
-    </section>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <section id="about">
+        <div className="wrapper">
+          <article>
+            <div className="title">
+              <h3>WHY JOIN E-CELL ?</h3>
+              <p className="separator" />
+            </div>
+            <div className="desc full">
+              <h4 className="subtitle">We teach you the Art to Sell</h4>
+              <p>
+                I suppose the answer is very obvious. You might be very good at
+                developing or competitive coding but, you are not good enough if
+                you don’t know how to sell it. To sell is one of the most
+                important skills one should have.
+              </p>
+              <p>
+                We, here at Entrepreneurship Cell, LNMIIT try to nurture the
+                entrepreneurial spirit amongst the students in college.
+              </p>
+            </div>
+            <div className="title">
+              <h3>Events</h3>
+              <p className="separator" />
+            </div>
+            <div className="desc">
+              <h4 className="subtitle">Shark Tank - LNM</h4>
+              <p>
+                The event is held during Plinth and is about providing a
+                platfrom to student-led startup founders where they can pitch
+                their idea in front of Angel Investors and Micro Venture
+                Capitalists
+              </p>
+              <p>
+                t this event, aspiring business owners from India present their
+                company models to a jury of potential investors and persuade
+                them to make financial investments in their venture.
+              </p>
+            </div>
+            <div className="desc">
+              <h4 className="subtitle">STARTUP WEEKEND JAIPUR</h4>
+              <p>
+                Startup Weekend aims to promote the Startup culture by providing
+                an ideal environment for startup magic to happen. Surrounded by
+                smart, passionate people and the right tools and approaches at
+                your disposal, you’ll take giant leaps towards creating a
+                business, becoming a founder, and connect with the right people
+                and resources.
+              </p>
+              <p>
+                If you have some skills and think that they might be handy for
+                somebody, then Techstars Startup Weekend is the right place for
+                you.
+              </p>
+            </div>
+            <div className="desc">
+              <h4 className="subtitle">YELLOW PAGES</h4>
+              <p>
+                ‘Yellow Pages’ is an inter-team challenge where we bound the
+                team with resources like time and budget and propel in the
+                people, a need to come up with a feasible idea to start within
+                the market. To ensure freshness and zest of the idea, newspapers
+                are given to look for a problem inside and device a solution.
+              </p>
+              <p>
+                This year an enormous number hit the board with 220 students
+                willing to share their business model on this platform in teams.
+                To guide them were 30 E-Cell student mentors, who have been in
+                close contact with ever growing entrepreneurship space.
+              </p>
+            </div>
+            <div className="desc">
+              <h4 className="subtitle">Sell with E-C­ell</h4>
+              <p>
+                Sell with E-C­ell main motive is to make the students of all the
+                year test their entrepreneurial skills and compete with each
+                other in order to win and prove themselves to be great
+                entrepreneurs. The event witnessed an overwhelming response from
+                the students and a significant amount of revenue Rs 65,000 was
+                generated during the four hours of the main event.
+              </p>
+              <p>
+                The event commenced with the auctioning of stalls where all the
+                teams appeared to bid for the stall as per its location.{" "}
+              </p>
+            </div>
+            <div className="desc">
+              <h4 className="subtitle">Virtual Stock Simulation Event</h4>
+              <p>
+                Virtual Stock Trading is a simulated trading process in which
+                new investors can practice investing without committing
+                money.Investing in Stock Market without prior knowledge will
+                most likely make you lose all yourinvestments. Only a few people
+                know the ins and outs of the stock game.So, before entering the
+                real market, you should have to trade virtually to understand
+                the trends and patterns of the stock price movement.
+              </p>
+              <p>
+                Virtual Stock Simulation helps new traders hit the ground
+                running with no capital risk, and real-time analysis and also
+                helps build confidence in budding investors.
+              </p>
+            </div>
+          </article>
+        </div>
+      </section>
+    </div>
+  );
+};
 
-export default Events
+export default Events;
