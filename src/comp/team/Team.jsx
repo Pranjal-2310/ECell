7a0c2457ed879b24a5e38bae38f53b551d3044bd
Hritul <hritul.srivastava@gmail.com>
2023-01-19 10:18:23 +0530

    12
    added the photos
    // still need to change links

--- src/comp/team/Team.jsx
+++ src/comp/team/Team.jsx
@@ -1,6 +1,17 @@
 import React from "react";
 import "./TeamStyle.css";
 
+import parv from "./IMG_20221228_133524.jpg";
+import naman from "./20220526_203035.jpg";
+import vaishvi from "./IMG_0327.jpg";
+import aryan from "./WhatsApp Image 2023-01-17 at 22.43.01.jpeg";
+import sarthak from "./20220125_225031.jpg";
+import hritul from "./IMG_20230117_200434.jpg";
+import kartik from "./IMG_1863.JPG";
+import linked from "./linkedin.png";
+import mail from "./email.png";
+import prakhar from "./IMG_20230117_014631.jpg";
+
 const Team = () => {
   return (
     <div>
@@ -10,21 +21,58 @@ const Team = () => {
             Our Team
           </p>
           <p class="text-blk sub-heading-text">
-          "None of us is as smart as all of us."
+            "None of us is as smart as all of us."
           </p>
           <div class="responsive-container-block cards-container">
             <div class="responsive-cell-block wk-desk-4 wk-ipadp-4 wk-mobile-12 wk-tab-12 card-container">
               <img
-                src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/eourInstructors1.svg"
+                src={vaishvi} 
+                class="team-member-image"
+              />
+              <p class="text-blk name">Vaishvi Bansal</p>
+              <p class="text-blk position">Cordinator Ecell</p>
+
+              <a href="https://www.linkedin.com/in/vaishvi-bansal-29a62a1bb/" target="_blank">
+                <img
+                  src={linked}
+                  class="social-media-icon"
+                  style={{ height: "20px", width: "20px" }}
+                />
+              </a>
+              <a href="https://www.facebook.com" target="_blank">
+                <img
+                  src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/gray-fb.svg"
+                  class="social-media-icon"
+                />
+              </a>
+              <a href="mailto:20uec143@lnmiit.ac.in" target="_blank">
+                <img
+                  src={mail}
+                  class="social-media-icon"
+                  style={{ height: "20px", width: "20px" }}
+                />
+              </a>
+              <a href="https://www.instagram.com" target="_blank">
+                <img
+                  src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/gray-insta.svg"
+                  class="social-media-icon"
+                />
+              </a>
+            
+            </div>
+            <div class="responsive-cell-block wk-desk-4 wk-ipadp-4 wk-mobile-12 wk-tab-12 card-container">
+              <img
+                src={sarthak} 
                 class="team-member-image"
               />
               <p class="text-blk name">Sarthak Kapoor</p>
               <p class="text-blk position">Cordinator Ecell</p>
 
-              <a href="https://www.twitter.com" target="_blank">
+              <a href="https://www.linkedin.com/in/sarthak-kapoor-686b381b8/" target="_blank">
                 <img
-                  src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/gray-twitter.svg"
+                  src={linked}
                   class="social-media-icon"
+                  style={{ height: "20px", width: "20px" }}
                 />
               </a>
               <a href="https://www.facebook.com" target="_blank">
@@ -33,10 +81,11 @@ const Team = () => {
                   class="social-media-icon"
                 />
               </a>
-              <a href="https://www.gmail.com" target="_blank">
+              <a href="mailto:ecell@lnmiit.ac.in" target="_blank">
                 <img
-                  src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/gray-mail.svg"
+                  src={mail}
                   class="social-media-icon"
+                  style={{ height: "20px", width: "20px" }}
                 />
               </a>
               <a href="https://www.instagram.com" target="_blank">
@@ -48,102 +97,217 @@ const Team = () => {
             </div>
             <div class="responsive-cell-block wk-desk-4 wk-ipadp-4 wk-mobile-12 wk-tab-12 card-container">
               <img
-                src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/eourInstructors2.svg"
+                src={parv} 
                 class="team-member-image"
               />
-              <p class="text-blk name">Sarthak Kapoor</p>
-              <p class="text-blk position">Cordinator Ecell</p>
-              <a href="https://www.twitter.com" target="_blank">
-                <img
-                  src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/gray-twitter.svg"
-                  class="social-media-icon"
-                />
-              </a>
-              <a href="https://www.facebook.com" target="_blank">
-                <img
-                  src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/gray-fb.svg"
-                  class="social-media-icon"
-                />
-              </a>
-              <a href="https://www.gmail.com" target="_blank">
-                <img
-                  src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/gray-mail.svg"
-                  class="social-media-icon"
-                />
-              </a>
-              <a href="https://www.instagram.com" target="_blank">
-                <img
-                  src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/gray-insta.svg"
-                  class="social-media-icon"
-                />
-              </a>
-            </div>
-            <div class="responsive-cell-block wk-desk-4 wk-ipadp-4 wk-mobile-12 wk-tab-12 card-container">
-              <img
-                src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/eourInstructors3.svg"
-                class="team-member-image"
-              />
-              <p class="text-blk name">Sarthak Kapoor</p>
-              <p class="text-blk position">Cordinator Ecell</p>
-              <a href="https://www.twitter.com" target="_blank">
-                <img
-                  src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/gray-twitter.svg"
-                  class="social-media-icon"
-                />
-              </a>
-              <a href="https://www.facebook.com" target="_blank">
-                <img
-                  src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/gray-fb.svg"
-                  class="social-media-icon"
-                />
-              </a>
-              <a href="https://www.gmail.com" target="_blank">
-                <img
-                  src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/gray-mail.svg"
-                  class="social-media-icon"
-                />
-              </a>
-              <a href="https://www.instagram.com" target="_blank">
-                <img
-                  src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/gray-insta.svg"
-                  class="social-media-icon"
-                />
-              </a>
-            </div>
-            <div class="responsive-cell-block wk-desk-4 wk-ipadp-4 wk-mobile-12 wk-tab-12 card-container">
-              <img
-                src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/eourInstructors3.svg"
-                class="team-member-image"
-              />
-              <p class="text-blk name">Sarthak Kapoor</p>
-              <p class="text-blk position">Cordinator Ecell</p>
-              <a href="https://www.twitter.com" target="_blank">
-                <img
-                  src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/gray-twitter.svg"
-                  class="social-media-icon"
-                />
-              </a>
-              <a href="https://www.facebook.com" target="_blank">
-                <img
-                  src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/gray-fb.svg"
-                  class="social-media-icon"
-                />
-              </a>
-              <a href="https://www.gmail.com" target="_blank">
-                <img
-                  src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/gray-mail.svg"
-                  class="social-media-icon"
-                />
-              </a>
-              <a href="https://www.instagram.com" target="_blank">
-                <img
-                  src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/gray-insta.svg"
-                  class="social-media-icon"
-                />
-              </a>
-            </div>
+              <p class="text-blk name">Parv Agarwal</p>
+              <p class="text-blk position">Core Member Ecell</p>
 
-            <div class="frame" >
+              <a href="https://www.linkedin.com/in/parvagarwal/" target="_blank">
+                <img
+                  src={linked}
+                  class="social-media-icon"
+                  style={{ height: "20px", width: "20px" }}
+                />
+              </a>
+              <a href="https://www.facebook.com" target="_blank">
+                <img
+                  src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/gray-fb.svg"
+                  class="social-media-icon"
+                />
+              </a>
+              <a href="mailto:ecell@lnmiit.ac.in" target="_blank">
+                <img
+                  src={mail}
+                  class="social-media-icon"
+                  style={{ height: "20px", width: "20px" }}
+                />
+              </a>
+              <a href="https://www.instagram.com" target="_blank">
+                <img
+                  src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/gray-insta.svg"
+                  class="social-media-icon"
+                />
+              </a>
+            </div>
+            <div class="responsive-cell-block wk-desk-4 wk-ipadp-4 wk-mobile-12 wk-tab-12 card-container">
+              <img
+                src={hritul} 
+                class="team-member-image"
+              />
+              <p class="text-blk name">Hritul Srivastava</p>
+              <p class="text-blk position">Core Member Ecell</p>
+
+              <a href="https://www.linkedin.com/in/hritul-srivastava-a0a27a20a/" target="_blank">
+                <img
+                  src={linked}
+                  class="social-media-icon"
+                  style={{ height: "20px", width: "20px" }}
+                />
+              </a>
+              <a href="https://www.facebook.com" target="_blank">
+                <img
+                  src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/gray-fb.svg"
+                  class="social-media-icon"
+                />
+              </a>
+              <a href="mailto:hritul.srivastava@gmail.com" target="_blank">
+                <img
+                  src={mail}
+                  class="social-media-icon"
+                  style={{ height: "20px", width: "20px" }}
+                />
+              </a>
+              <a href="https://www.instagram.com" target="_blank">
+                <img
+                  src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/gray-insta.svg"
+                  class="social-media-icon"
+                />
+              </a>
+            </div>
+            <div class="responsive-cell-block wk-desk-4 wk-ipadp-4 wk-mobile-12 wk-tab-12 card-container">
+              <img
+                src={aryan} 
+                class="team-member-image"
+              />
+              <p class="text-blk name">Aryan Kasat</p>
+              <p class="text-blk position">Core Member Ecell</p>
+
+              <a href="https://www.linkedin.com/in/aryan-kasat-539377211/" target="_blank">
+                <img
+                  src={linked}
+                  class="social-media-icon"
+                  style={{ height: "20px", width: "20px" }}
+                />
+              </a>
+              <a href="https://www.facebook.com" target="_blank">
+                <img
+                  src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/gray-fb.svg"
+                  class="social-media-icon"
+                />
+              </a>
+              <a href="mailto:hritul.srivastava@gmail.com" target="_blank">
+                <img
+                  src={mail}
+                  class="social-media-icon"
+                  style={{ height: "20px", width: "20px" }}
+                />
+              </a>
+              <a href="https://www.instagram.com" target="_blank">
+                <img
+                  src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/gray-insta.svg"
+                  class="social-media-icon"
+                />
+              </a>
+            </div>
+            <div class="responsive-cell-block wk-desk-4 wk-ipadp-4 wk-mobile-12 wk-tab-12 card-container">
+              <img
+                src={naman} 
+                class="team-member-image"
+              />
+              <p class="text-blk name">Naman Goyal</p>
+              <p class="text-blk position">Core Member Ecell</p>
+
+              <a href="https://www.linkedin.com/in/nammangoyal/" target="_blank">
+                <img
+                  src={linked}
+                  class="social-media-icon"
+                  style={{ height: "20px", width: "20px" }}
+                />
+              </a>
+              <a href="https://www.facebook.com" target="_blank">
+                <img
+                  src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/gray-fb.svg"
+                  class="social-media-icon"
+                />
+              </a>
+              <a href="mailto:hritul.srivastava@gmail.com" target="_blank">
+                <img
+                  src={mail}
+                  class="social-media-icon"
+                  style={{ height: "20px", width: "20px" }}
+                />
+              </a>
+              <a href="https://www.instagram.com" target="_blank">
+                <img
+                  src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/gray-insta.svg"
+                  class="social-media-icon"
+                />
+              </a>
+            </div>
+            <div class="responsive-cell-block wk-desk-4 wk-ipadp-4 wk-mobile-12 wk-tab-12 card-container">
+              <img
+                src={prakhar} 
+                class="team-member-image"
+              />
+              <p class="text-blk name">Prakhar Gupta</p>
+              <p class="text-blk position">Core Member Ecell</p>
+
+              <a href="https://www.linkedin.com/in/prakhar-gupta-fanperk/" target="_blank">
+                <img
+                  src={linked}
+                  class="social-media-icon"
+                  style={{ height: "20px", width: "20px" }}
+                />
+              </a>
+              <a href="https://www.facebook.com" target="_blank">
+                <img
+                  src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/gray-fb.svg"
+                  class="social-media-icon"
+                />
+              </a>
+              <a href="mailto:hritul.srivastava@gmail.com" target="_blank">
+                <img
+                  src={mail}
+                  class="social-media-icon"
+                  style={{ height: "20px", width: "20px" }}
+                />
+              </a>
+              <a href="https://www.instagram.com" target="_blank">
+                <img
+                  src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/gray-insta.svg"
+                  class="social-media-icon"
+                />
+              </a>
+            </div>
+            <div class="responsive-cell-block wk-desk-4 wk-ipadp-4 wk-mobile-12 wk-tab-12 card-container">
+              <img
+                src={kartik} 
+                class="team-member-image"
+              />
+              <p class="text-blk name">Kartik Rawat</p>
+              <p class="text-blk position">Core Member Ecell</p>
+
+              <a href="https://www.linkedin.com/in/kartik-rawat-fanperk/" target="_blank">
+                <img
+                  src={linked}
+                  class="social-media-icon"
+                  style={{ height: "20px", width: "20px" }}
+                />
+              </a>
+              <a href="https://www.facebook.com" target="_blank">
+                <img
+                  src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/gray-fb.svg"
+                  class="social-media-icon"
+                />
+              </a>
+              <a href="mailto:hritul.srivastava@gmail.com" target="_blank">
+                <img
+                  src={mail}
+                  class="social-media-icon"
+                  style={{ height: "20px", width: "20px" }}
+                />
+              </a>
+              <a href="https://www.instagram.com" target="_blank">
+                <img
+                  src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/gray-insta.svg"
+                  class="social-media-icon"
+                />
+              </a>
+            </div>
+           
+
+            <div class="frame">
               <button class="custom-btn btn">Read More</button>
             </div>
           </div>
